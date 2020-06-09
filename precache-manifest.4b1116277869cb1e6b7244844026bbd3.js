885fff530d4aa3872856348ca84b350678199646
alem-alem <boricalem@gmail.com>
2020-06-09 08:04:13 +0200

    Updates

---
+++ precache-manifest.4b1116277869cb1e6b7244844026bbd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0482f98375c2bedaab690079201bd5db",
+    "url": "/ReactJS-Weather-App/index.html"
+  },
+  {
+    "revision": "b407a0e09a23f26890f4",
+    "url": "/ReactJS-Weather-App/static/css/main.565188bb.chunk.css"
+  },
+  {
+    "revision": "a599a46dfd772b1fe8bd",
+    "url": "/ReactJS-Weather-App/static/js/2.f3bb244c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS-Weather-App/static/js/2.f3bb244c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b407a0e09a23f26890f4",
+    "url": "/ReactJS-Weather-App/static/js/main.5e0bc3ed.chunk.js"
+  },
+  {
+    "revision": "91430d659752ef7c046c",
+    "url": "/ReactJS-Weather-App/static/js/runtime-main.a92b1362.js"
+  },
+  {
+    "revision": "ca6e6883c94274f6887a8cc7b0f895ab",
+    "url": "/ReactJS-Weather-App/static/media/weather0.ca6e6883.jpg"
+  },
+  {
+    "revision": "e11d8d687e6cc9b14d1e21ce266597b9",
+    "url": "/ReactJS-Weather-App/static/media/weather1.e11d8d68.jpg"
+  }
+]);
